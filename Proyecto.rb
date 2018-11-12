68ce3d351481250f49e0c6d0126bd22e4ee1c281
Kelvin <allecan@52gmail.com>
2018-11-12 16:57:38 -0600

    commit

--- Proyecto.rb
+++ Proyecto.rb
@@ -1,34 +1,29 @@
 require 'byebug'
-begin
+
 
 def limpiar_pantalla
-
     system ("clear")
-
 end
 
+def ingreso_de_datos 
+    limpiar_pantalla()
+    puts 'Ingrese un arreglo de numeros para trabajar:'
+    @arreglo = gets.chomp.split(',')
+end
 
-    def ingreso_de_datos 
-
-limpiar_pantalla()
-        puts 'Ingrese un arreglo de numeros para trabajar:'
-        @arreglo = gets.chomp.split(',')
-        limpiar_pantalla()
-    end
-
+begin
     puts '1. Ingreso de numeros'
     puts '2. Ejecutar ordenamiento'
     puts '3. Ordenar paso a paso'
     puts '4. salir'
     opcion = gets.chomp
+    limpiar_pantalla
     if opcion == '1'
-
-     ingreso_de_datos()
-
+     ingreso_de_datos
     elsif opcion == '2'
-
-        puts "#{@arreglo} "
-
+        puts "#{@arreglo}"
+        gets
     elsif opcion == '3'
     end
+    limpiar_pantalla
 end while opcion != '4'
