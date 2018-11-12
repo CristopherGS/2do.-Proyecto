97bd58d1f331c1e94d15d7fb963fca26fedaeb40
KhrizGS <guerracristofer@gmail.com>
2018-11-12 16:21:03 -0600

    main

---
+++ Proyecto.rb
@@ -0,0 +1,14 @@
+require 'byebug'
+begin
+    puts '1. Ingreso de numeros'
+    puts '2. Ejecutar ordenamiento'
+    puts '3. Ordenar paso a paso'
+    puts '4. salir'
+    opcion = gets.chomp
+    if opcion == '1'
+
+    elsif opcion == '2'
+
+    elsif opcion == '3'
+    end
+end while opcion != 4 
