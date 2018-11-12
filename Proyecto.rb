ebc6a3196e13bda6c5d8a1aaa3d0dbab6f868e03
Luis <ljrp30@gmail.com>
2018-11-12 16:43:13 -0600

    Ingreso de datos

--- Proyecto.rb
+++ Proyecto.rb
@@ -1,5 +1,21 @@
 require 'byebug'
 begin
+
+def limpiar_pantalla
+
+    system ("clear")
+
+end
+
+
+    def ingreso_de_datos 
+
+limpiar_pantalla()
+        puts 'Ingrese un arreglo de numeros para trabajar:'
+        @arreglo = gets.chomp.split(',')
+        limpiar_pantalla()
+    end
+
     puts '1. Ingreso de numeros'
     puts '2. Ejecutar ordenamiento'
     puts '3. Ordenar paso a paso'
@@ -7,8 +23,12 @@ begin
     opcion = gets.chomp
     if opcion == '1'
 
+     ingreso_de_datos()
+
     elsif opcion == '2'
+
+        puts "#{@arreglo} "
 
     elsif opcion == '3'
     end
-end while opcion != 4 
+end while opcion != '4'
