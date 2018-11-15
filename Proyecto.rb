8a6025101501ed96e1c845e470fd065671a68b5b
Donald Abac <ddaniel_abac@outlook.es>
2018-11-14 20:22:14 -0600

    actualizacion de listas

--- Proyecto.rb
+++ Proyecto.rb
@@ -1,5 +1,14 @@
 require 'byebug'
-
+require_relative 'clases listas'
+#array=[]
+#listas=Listas.new()
+#def ingresar_datos(array=[])
+#   print 'Ingrese los numreso: '
+#   array=gets.chomp.split(' ').map(&:to_i)
+#end
+array = ingresar_datos(array)
+listas.agregar(array)
+listas.mostrar()
 
 def limpiar_pantalla
     system ("clear")
